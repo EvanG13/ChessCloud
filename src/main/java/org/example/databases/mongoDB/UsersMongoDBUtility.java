31025bbdc4e98f17eac76fcea444bd011e116852
EvanG13 <emgiampaoli@gmail.com>
2024-04-12 20:58:43 -0700

    feat: create Mongodb wrappers (#9)

---
+++ src/main/java/org/example/databases/mongoDB/UsersMongoDBUtility.java
@@ -0,0 +1,49 @@
+package org.example.databases.mongoDB;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.example.databases.MongoDBUtility;
+import org.example.entities.User;
+import org.example.requestRecords.UserRequest;
+
+public class UsersMongoDBUtility {
+    private final MongoDBUtility utility;
+
+    // TODO : https://github.com/EvanG13/ChessCloud/issues/10
+    public UsersMongoDBUtility(MongoDBUtility utility) {
+        this.utility = utility;
+    }
+
+    /**
+     * Get a User by their id
+     * @param id id
+     * @return user
+     */
+    public User get(String id) {
+        Document doc = utility.get(id);
+        if (doc == null) {
+            return null;
+        }
+
+        return User.fromDocument(doc);
+    }
+
+    /**
+     * Create a new User
+     * @param userData user request data object
+     */
+    public void post(UserRequest userData) {
+        utility.post(new Document("_id", new ObjectId())
+                .append("email", userData.email())
+                .append("password", userData.password())
+        );
+    }
+
+    /**
+     * Deletes a User by their id
+     * @param id id
+     */
+    public void delete(String id) {
+        utility.delete(id);
+    }
+}
