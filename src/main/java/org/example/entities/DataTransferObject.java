26b2a1a60064944a5f38c4e64d4b019cbdf3b223
Evan <45468330+EvanG13@users.noreply.github.com>
2024-04-13 17:47:50 -0700

    feat: MongoDB wrapper class (#9, PR #11)
    * feat: started mongodb service setup
    
    * chore: refactor
    
    * test: add db tests (#9)
    
    * feat: add user record and dto (#9)
    
    * feat: create Mongodb wrappers (#9)
    
    * chore: cleanup tests (#9)

---
+++ src/main/java/org/example/entities/DataTransferObject.java
@@ -0,0 +1,21 @@
+package org.example.entities;
+
+import org.bson.Document;
+
+public abstract class DataTransferObject {
+    protected String id;
+
+    public DataTransferObject(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Convert object to a BSON Document
+     * @return BSON Document
+     */
+    public abstract Document toDocument();
+}
---
+++ src/main/java/org/example/entities/User.java
@@ -0,0 +1,37 @@
+package org.example.entities;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+public class User extends DataTransferObject {
+    private final String email;
+    private final String password;
+
+    public User(String id, String email, String password) {
+        super(id);
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public Document toDocument() {
+        return new Document("_id", new ObjectId(id))
+                .append("email", email)
+                .append("password", password);
+    }
+
+    public static User fromDocument(Document userDocument) {
+        return new User(
+                String.valueOf(userDocument.getObjectId("_id")),
+                userDocument.getString("email"),
+                userDocument.getString("password")
+        );
+    }
+}
