8328aac6edb0dd0b439bec6004dcb08eb7dd1b72
Evan <45468330+EvanG13@users.noreply.github.com>
2024-04-17 16:56:56 -0700

    feat: 5 decide on and setup db collections (#18)
    * feat: create users dynamo db table with terraform
    
    * chore: reformat all src code to google standard
    
    * feat: create users dynamo db table with terraform
    
    * chore: fmt terraform
    
    ---------
    
    Co-authored-by: CleveyP <clevelandplonsey@gmail.com>

--- src/main/java/org/example/entities/DataTransferObject.java
+++ src/main/java/org/example/entities/DataTransferObject.java
@@ -3,19 +3,20 @@ package org.example.entities;
 import org.bson.Document;
 
 public abstract class DataTransferObject {
-    protected String id;
+  protected String id;
 
-    public DataTransferObject(String id) {
-        this.id = id;
-    }
+  public DataTransferObject(String id) {
+    this.id = id;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    /**
-     * Convert object to a BSON Document
-     * @return BSON Document
-     */
-    public abstract Document toDocument();
+  /**
+   * Convert object to a BSON Document
+   *
+   * @return BSON Document
+   */
+  public abstract Document toDocument();
 }
