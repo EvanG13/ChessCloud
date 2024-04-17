4cbdf31a0c4c5383bbaa6b1955ccbe8c80e568e6
EvanG13 <emgiampaoli@gmail.com>
2024-04-17 16:36:25 -0700

    feat: update login

--- src/test/java/org/example/utils/EncryptPasswordTest.java
+++ src/test/java/org/example/utils/EncryptPasswordTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class EncryptPasswordTest {
   @Test
   public void canEncryptPassword() {
