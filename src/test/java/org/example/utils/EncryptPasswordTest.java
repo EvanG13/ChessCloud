c75329477975de4733c974cf09c0cd047b8b2219
EvanG13 <emgiampaoli@gmail.com>
2024-04-20 15:22:19 -0700

    test: update and add tests (#10)

--- src/test/java/org/example/utils/EncryptPasswordTest.java
+++ src/test/java/org/example/utils/EncryptPasswordTest.java
@@ -3,13 +3,18 @@ package org.example.utils;
 import static org.junit.jupiter.api.Assertions.*;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class EncryptPasswordTest {
+
+  @DisplayName("Password Encryption")
   @Test
-  public void canEncryptPassword() {
-    String password = "newpassword";
+  public void canEncryptAndVerifyPassword() {
+    String password = "testPassword";
     String encryptedPass = EncryptPassword.encrypt(password);
+
+    System.out.println(encryptedPass);
 
     assertNotNull(encryptedPass);
 
