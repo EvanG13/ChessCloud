28ca40f79ef131bd8e16cf458927dd1c65e887e8
EvanG13 <emgiampaoli@gmail.com>
2024-04-19 19:19:50 -0700

    test: update login test

--- src/test/java/org/example/handlers/login/LoginHandlerTest.java
+++ src/test/java/org/example/handlers/login/LoginHandlerTest.java
@@ -1,6 +1,6 @@
 package org.example.handlers.login;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -8,6 +8,8 @@ import static org.mockito.Mockito.when;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.example.databases.users.UsersDynamoDBUtility;
 import org.example.entities.User;
 import org.example.handlers.TestContext;
@@ -49,6 +51,24 @@ public class LoginHandlerTest {
             new User(
                 "foo", "nonexistingemail@example.com", EncryptPassword.encrypt("test"), "fake"));
     APIGatewayV2HTTPResponse response = loginHandler.handleRequest(event, context);
+
+    System.out.println(response.getBody());
+
+    // The response Body contains the expected fields
+    assertTrue(response.getBody().contains("jwt"));
+    assertTrue(response.getBody().contains("user"));
+
+    String body = response.getBody();
+    Gson gson = new Gson();
+    JsonObject jsonObject = gson.fromJson(body, JsonObject.class);
+    String userJsonString = jsonObject.get("user").getAsString();
+    User user = gson.fromJson(userJsonString, User.class);
+
+    // The user response object contains the correct fields
+    assertEquals(user.getId(), "foo");
+    assertEquals(user.getUsername(), "fake");
+    assertEquals(user.getEmail(), "nonexistingemail@example.com");
+    assertNull(user.getPassword());
 
     assertEquals(StatusCodes.OK, response.getStatusCode());
   }
