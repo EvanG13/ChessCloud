9d27186d2aeb469c900d38e7dcd96e25f2a6068d
EvanG13 <emgiampaoli@gmail.com>
2024-04-30 15:28:29 -0700

    infra: Delete dynamodb infra (#27)

--- src/main/java/org/example/entities/DataTransferObject.java
+++ src/main/java/org/example/entities/DataTransferObject.java
@@ -2,26 +2,15 @@ package org.example.entities;
 
 import com.google.gson.annotations.Expose;
 import org.bson.Document;
-import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
-import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
-import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 
-@DynamoDbBean
 public abstract class DataTransferObject {
 
   @Expose protected String id;
-
-  /**
-   * !Please do NOT remove this. The DynamoDB enhanced client requires having a default constructor
-   */
-  public DataTransferObject() {}
 
   public DataTransferObject(String id) {
     this.id = id;
   }
 
-  @DynamoDbPartitionKey
-  @DynamoDbAttribute(value = "id")
   public String getId() {
     return this.id;
   }
