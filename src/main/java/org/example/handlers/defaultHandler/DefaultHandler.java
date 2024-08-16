b25f7677af985a059eb03b18ca09541066e5b36d
kysJune <90539840+kysJune@users.noreply.github.com>
2024-08-15 17:19:51 -0700

    implented defaulthandler

--- src/main/java/org/example/handlers/defaultHandler/DefaultHandler.java
+++ src/main/java/org/example/handlers/defaultHandler/DefaultHandler.java
@@ -1,3 +1,25 @@
 package org.example.handlers.defaultHandler;
 
-public class DefaultHandler {}
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2WebSocketEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2WebSocketResponse;
+import org.example.statusCodes.StatusCodes;
+
+public class DefaultHandler
+    implements RequestHandler<APIGatewayV2WebSocketEvent, APIGatewayV2WebSocketResponse> {
+  @Override
+  public APIGatewayV2WebSocketResponse handleRequest(
+      APIGatewayV2WebSocketEvent event, Context context) {
+    // TODO: save this to the connections mongo table
+    APIGatewayV2WebSocketResponse response = new APIGatewayV2WebSocketResponse();
+    response.setStatusCode(StatusCodes.BAD_REQUEST);
+
+    // Gson gson = new Gson();
+    // DefaultRequest defaultRequest = gson.fromJson(event.getBody(), DefaultRequest.class);
+
+    response.setBody("Route: " + event.getBody() + " is not supported");
+
+    return response;
+  }
+}
