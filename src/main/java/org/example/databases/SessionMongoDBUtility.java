53738ef2c2ea0a6cfae8344304e2aafea61f8a20
kysJune <90539840+kysJune@users.noreply.github.com>
2024-08-21 19:18:39 -0700

    formatting

--- src/main/java/org/example/databases/SessionMongoDBUtility.java
+++ src/main/java/org/example/databases/SessionMongoDBUtility.java
@@ -2,57 +2,52 @@ package org.example.databases;
 
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
-import org.bson.types.ObjectId;
 import org.example.entities.Session;
-import org.example.entities.User;
 import org.example.requestRecords.SessionRequest;
-import org.example.requestRecords.UserRequest;
 
 public class SessionMongoDBUtility {
-    private final MongoDBUtility utility;
+  private final MongoDBUtility utility;
 
-    public SessionMongoDBUtility() {
-        utility = MongoDBUtility.getInstance("sessions");
+  public SessionMongoDBUtility() {
+    utility = MongoDBUtility.getInstance("sessions");
+  }
+
+  public SessionMongoDBUtility(MongoDBUtility utility) {
+    this.utility = utility;
+  }
+
+  /**
+   * Get Session by sessionId, returns null if sessionId is invalid
+   *
+   * @param id sessionId
+   * @return Session
+   */
+  public Session get(String id) {
+    // Document doc = utility.get(id);
+    Document doc = utility.get(Filters.eq("_id", id));
+
+    if (doc == null) {
+      return null;
     }
 
-    public SessionMongoDBUtility(MongoDBUtility utility) {
-        this.utility = utility;
-    }
+    return Session.fromDocument(doc);
+  }
 
-    /**
-     * Get Session by sessionId, returns null if sessionId is invalid
-     *
-     * @param id sessionId
-     * @return Session
-     */
-    public Session get(String id) {
-        //Document doc = utility.get(id);
-        Document doc = utility.get(Filters.eq("_id", id));
+  /**
+   * Create a new Session
+   *
+   * @param sessionData session request data object
+   */
+  public void post(SessionRequest sessionData) {
+    utility.post(new Document("_id", sessionData.id()).append("userId", sessionData.userId()));
+  }
 
-        if (doc == null) {
-            return null;
-        }
-
-        return Session.fromDocument(doc);
-    }
-
-    /**
-     * Create a new Session
-     *
-     * @param sessionData session request data object
-     */
-    public void post(SessionRequest sessionData) {
-        utility.post(
-                new Document("_id", sessionData.id())
-                        .append("userId", sessionData.userId()));
-    }
-
-    /**
-     * Deletes a User by their id
-     *
-     * @param id id
-     */
-    public void delete(String id) {
-        utility.deleteByIndex("_id", id);
-    }
+  /**
+   * Deletes a User by their id
+   *
+   * @param id id
+   */
+  public void delete(String id) {
+    utility.deleteByIndex("_id", id);
+  }
 }
--- src/main/java/org/example/entities/Session.java
+++ src/main/java/org/example/entities/Session.java
@@ -7,32 +7,29 @@ import org.bson.Document;
 import org.bson.types.ObjectId;
 
 public class Session extends DataTransferObject {
-    @Expose String userId;
+  @Expose String userId;
 
-    public Session(String id, String userId) {
-        super(id);
-        this.userId = userId;
-    }
+  public Session(String id, String userId) {
+    super(id);
+    this.userId = userId;
+  }
 
-    public static Session fromDocument(Document sessionDocument) {
-        return new Session(
-                    sessionDocument.getString("_id"),
-                    sessionDocument.getString("userId"));
-    }
+  public static Session fromDocument(Document sessionDocument) {
+    return new Session(sessionDocument.getString("_id"), sessionDocument.getString("userId"));
+  }
 
-    public Document toDocument() {
-        return new Document("_id", new ObjectId(id));
-    }
+  public Document toDocument() {
+    return new Document("_id", new ObjectId(id));
+  }
 
-    public String toString(){
-        return this.id + " " + this.userId;
-    }
+  public String toString() {
+    return this.id + " " + this.userId;
+  }
 
-    @Override
-    public String toResponseJson() {
-        Gson gsonBuilder = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+  @Override
+  public String toResponseJson() {
+    Gson gsonBuilder = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 
-        return gsonBuilder.toJson(this, Session.class);
-    }
+    return gsonBuilder.toJson(this, Session.class);
+  }
 }
-
