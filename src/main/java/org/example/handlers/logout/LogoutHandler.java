483bd87e8c6a929d4929a3ef2add772609e5bf05
EvanG13 <emgiampaoli@gmail.com>
2024-09-04 20:50:18 -0700

    feat: create auth lambda (#15)

--- src/main/java/org/example/handlers/logout/LogoutHandler.java
+++ src/main/java/org/example/handlers/logout/LogoutHandler.java
@@ -4,14 +4,12 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import com.google.gson.Gson;
-import org.example.requestRecords.LogoutRequest;
 import org.example.statusCodes.StatusCodes;
 import org.example.utils.AuthHeaders;
 
 public class LogoutHandler
     implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
-  private LogoutService service;
+  private final LogoutService service;
 
   public LogoutHandler() {
     service = new LogoutService();
@@ -23,18 +21,21 @@ public class LogoutHandler
 
   @Override
   public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
-    if (event == null || event.getBody() == null) {
+    if (event == null || event.getHeaders() == null) {
       return APIGatewayV2HTTPResponse.builder()
           .withHeaders(AuthHeaders.getCorsHeaders())
           .withBody("Missing Event object or Event request body")
           .withStatusCode(StatusCodes.BAD_REQUEST)
           .build();
     }
-    Gson gson = new Gson();
-    String sessionToken = gson.fromJson(event.getBody(), LogoutRequest.class).sessionToken();
+
+    String sessionToken =
+        event.getHeaders().get("Authorization").replace("Bearer ", "").replace("\"", "");
+
     service.destroySession(sessionToken);
+
     return APIGatewayV2HTTPResponse.builder()
-        .withBody(new LogoutService().getMessage())
+        .withBody("Logged out successfully.")
         .withStatusCode(StatusCodes.OK)
         .build();
   }
