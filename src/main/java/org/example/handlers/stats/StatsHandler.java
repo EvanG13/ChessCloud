d94d0378160ee4c392a458e8bac90430934bb6c9
CaiminRO <89493663+CaiminRO@users.noreply.github.com>
2024-09-15 00:39:57 -0700

    feat: split user Stats for each game mode (#65)
    * feat: add user Stats per GameMode
    
    * chore: group TimeControls under respective GameMode
    
    * chore: init Stats entity for registered user
    
    * chore: get GameMode rating from Stats entity on join game
    
    * Rebased with main after PR #57 was merged
    
    * fix: Join Game Tests now use Stats entity
    
    * fix: made gameModeStats map use lowercased keys
    
    * feat: funcs to check or get stats for game mode in Stats entity
    
    * feat: overloaded func to get rating from GameMode as string
    
    * feat: can get stats for game mode(s) in JSON string
    
    * chore: updated rebased StatsHandler to return JSON stats for game mode(s)
    
    * chore: updated expected response format for stats route
    
    * feat: added tests for updated StatsHandler
    
    * Automated reformatting after successful build (with tests)

--- src/main/java/org/example/handlers/stats/StatsHandler.java
+++ src/main/java/org/example/handlers/stats/StatsHandler.java
@@ -6,7 +6,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import java.util.Map;
 import java.util.Optional;
+import org.example.entities.Stats;
 import org.example.entities.User;
 import org.example.statusCodes.StatusCodes;
 
@@ -26,14 +28,54 @@ public class StatsHandler
   public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
     String userId = event.getHeaders().get("userid");
 
-    Optional<User> optionalUser = service.getByID(userId);
+    // get query parameters
+    Map<String, String> queryParams = event.getQueryStringParameters();
 
+    // check user exists
+    Optional<User> optionalUser = service.getUserByID(userId);
     if (optionalUser.isEmpty()) {
       return makeHttpResponse(StatusCodes.BAD_REQUEST, "Missing User");
     }
 
-    User user = optionalUser.get();
+    // check user has stats
+    Optional<Stats> optionalUserStats = service.getStatsByUserID(userId);
+    if (optionalUserStats.isEmpty()) {
+      return makeHttpResponse(StatusCodes.INTERNAL_SERVER_ERROR, "Missing User's Stats");
+    }
 
-    return makeHttpResponse(StatusCodes.OK, user.toStatsJSON());
+    Stats userStats = optionalUserStats.get();
+
+    // if query specified
+    if (queryParams != null) {
+      // try to get possible query parameters
+      String queryGamemode = queryParams.get("gamemode");
+
+      // gamemode not part of query: bad request
+      if (queryGamemode == null) {
+        return makeHttpResponse(
+            StatusCodes.BAD_REQUEST, "Query defined, but query parameter \"gamemode\" was missing");
+      }
+
+      // missing value for gamemode: bad request
+      if (queryGamemode.isEmpty()) {
+        return makeHttpResponse(
+            StatusCodes.BAD_REQUEST, "Query parameter \"gamemode\" was missing a value");
+      }
+
+      Optional<String> optionalJson = userStats.toJSON(queryGamemode);
+
+      // invalid value for gamemode: bad request
+      if (optionalJson.isEmpty()) {
+        return makeHttpResponse(
+            StatusCodes.BAD_REQUEST,
+            "Query parameter \"gamemode\" had an invalid value: " + queryGamemode);
+      }
+
+      // return with QUERIED gamemode
+      return makeHttpResponse(StatusCodes.OK, optionalJson.get());
+    }
+
+    // return with ALL gamemodes
+    return makeHttpResponse(StatusCodes.OK, userStats.toJSON());
   }
 }
