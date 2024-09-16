ad849d09fc1aad46715549729b7ad43840447c18
CleveyP <clevelandplonsey@gmail.com>
2024-09-16 05:24:10 -0700

    feat:implement Constants class

--- src/main/java/org/example/utils/Constants.java
+++ src/main/java/org/example/utils/Constants.java
@@ -1,2 +1,8 @@
-package org.example.utils;public class Constants {
+package org.example.utils;
+
+public class Constants {
+  public static final Integer BASE_RATING = 1000;
+  public static final Double BASE_RD = 350.0;
+  public static final Double Q =
+      Math.log(10) / 400; // scaling factor constant used in calculating glicko ratings
 }
