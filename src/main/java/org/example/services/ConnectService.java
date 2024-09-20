cf97e7f3464905045e21f08cca8d186c4cb840a0
CleveyP <clevelandplonsey@gmail.com>
2024-09-19 22:13:02 -0700

    feat: add updateConnectionId function to ConnectService

--- src/main/java/org/example/services/ConnectService.java
+++ src/main/java/org/example/services/ConnectService.java
@@ -1,18 +1,24 @@
 package org.example.services;
 
 import com.mongodb.client.model.Filters;
+import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
+import org.bson.conversions.Bson;
 import org.example.entities.Connection;
+import org.example.entities.Game;
+import org.example.entities.Player;
 import org.example.models.requests.ConnectionRequest;
 import org.example.utils.MongoDBUtility;
 
 @AllArgsConstructor
 public class ConnectService {
   private final MongoDBUtility<Connection> utility;
+  private final MongoDBUtility<Game> gameMongoDBUtility;
 
   public ConnectService() {
     this.utility = new MongoDBUtility<>("connections", Connection.class);
+    this.gameMongoDBUtility = new MongoDBUtility<>("games", Game.class);
   }
 
   public boolean doesConnectionExistByUsername(String username) {
@@ -33,5 +39,23 @@ public class ConnectService {
         Connection.builder().id(data.connectionId()).username(data.username()).build();
 
     utility.post(newConnection);
+  }
+
+  public void updateConnectionId(String userId, String connectionId) {
+    // find out if user is in a game
+    Bson filter = Filters.elemMatch("players", Filters.eq("playerId", userId));
+    Optional<Game> optionalGame = gameMongoDBUtility.get(filter);
+    if (optionalGame.isEmpty()) {
+      return;
+    }
+    Game game = optionalGame.get();
+    List<Player> playerList = game.getPlayers();
+    if (playerList.get(0).getPlayerId().equals(userId)) {
+      playerList.get(0).setConnectionId(connectionId);
+    } else {
+      playerList.get(1).setConnectionId(connectionId);
+    }
+    game.setPlayers(playerList);
+    gameMongoDBUtility.put(game.getId(), game);
   }
 }
