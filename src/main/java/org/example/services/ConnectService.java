dc90dfecb9df031b131fbf5cd324c4a895162d20
CleveyP <clevelandplonsey@gmail.com>
2024-09-19 22:21:34 -0700

    remove all traces of Connection entity from ConnectService as the Connection entity is deprecated

--- src/main/java/org/example/services/ConnectService.java
+++ src/main/java/org/example/services/ConnectService.java
@@ -5,42 +5,18 @@ import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.bson.conversions.Bson;
-import org.example.entities.Connection;
 import org.example.entities.Game;
 import org.example.entities.Player;
-import org.example.models.requests.ConnectionRequest;
 import org.example.utils.MongoDBUtility;
 
 @AllArgsConstructor
 public class ConnectService {
-  private final MongoDBUtility<Connection> utility;
+
   private final MongoDBUtility<Game> gameMongoDBUtility;
 
   public ConnectService() {
-    this.utility = new MongoDBUtility<>("connections", Connection.class);
     this.gameMongoDBUtility = new MongoDBUtility<>("games", Game.class);
   }
-
-  public boolean doesConnectionExistByUsername(String username) {
-    Optional<Connection> conn = utility.get(Filters.eq("username", username));
-
-    return conn.isPresent();
-  }
-
-  public boolean doesConnectionExistById(String connectionId) {
-    Optional<Connection> conn = utility.get(connectionId);
-
-    return conn.isPresent();
-  }
-
-  public void createConnection(ConnectionRequest data) {
-
-    Connection newConnection =
-        Connection.builder().id(data.connectionId()).username(data.username()).build();
-
-    utility.post(newConnection);
-  }
-
   public void updateConnectionId(String userId, String connectionId) {
     // find out if user is in a game
     Bson filter = Filters.elemMatch("players", Filters.eq("playerId", userId));
