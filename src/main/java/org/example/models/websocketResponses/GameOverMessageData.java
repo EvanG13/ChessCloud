0ce915c8f8afd524f9c8870538adbf7df103aaa0
CleveyP <clevelandplonsey@gmail.com>
2024-09-27 19:56:59 -0700

    refactor: GameOverResponseBody into GameOverMessageData and convert switch statement to new java 14 syntax

---
+++ src/main/java/org/example/models/websocketResponses/GameOverMessageData.java
@@ -0,0 +1,36 @@
+package org.example.models.websocketResponses;
+
+import com.google.gson.annotations.Expose;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import org.example.enums.ResultReason;
+import org.example.exceptions.InternalServerError;
+
+@Getter
+@SuperBuilder
+public class GameOverMessageData extends SocketMessageData {
+  @Expose private final ResultReason resultReason;
+  @Expose private final String displayMessage;
+  @Expose private final String winnerUsername;
+  @Expose private final String loserUsername;
+
+  public GameOverMessageData(ResultReason resultReason, String winnerUsername, String loserUsername)
+      throws InternalServerError {
+    super(true, "game ended");
+    this.resultReason = resultReason;
+    this.winnerUsername = winnerUsername;
+    this.loserUsername = loserUsername;
+
+    switch (resultReason) {
+      case ABORTED ->
+          this.displayMessage =
+              String.format("Game invalid. Reason: %s", resultReason.getMessage());
+      case FORFEIT, TIMEOUT, CHECKMATE ->
+          this.displayMessage =
+              String.format("%s won! Reason: %s", winnerUsername, resultReason.getMessage());
+      case REPETITION, INSUFFICIENT_MATERIAL ->
+          this.displayMessage = String.format("Draw! Reason: %s", resultReason.getMessage());
+      default -> throw new InternalServerError("Unsupported ResultReason: " + resultReason);
+    }
+  }
+}
