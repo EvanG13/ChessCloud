920c5e1bdaedf59041427dfb62c8ccb4c78f0034
CleveyP <clevelandplonsey@gmail.com>
2024-09-27 19:55:26 -0700

    create: GameCreatedMessageData class

---
+++ src/main/java/org/example/models/websocketResponses/GameCreatedMessageData.java
@@ -0,0 +1,12 @@
+package org.example.models.websocketResponses;
+
+
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+public class GameCreatedMessageData extends SocketMessageData {
+
+  public GameCreatedMessageData() {
+    super(true, "game created!");
+  }
+}
