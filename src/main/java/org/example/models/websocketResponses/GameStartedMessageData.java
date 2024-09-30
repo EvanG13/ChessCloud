25d6978160b0d3a1393c8920d50b18ac764e3662
CleveyP <clevelandplonsey@gmail.com>
2024-09-30 04:51:52 -0700

    fix: GameStartedMessageData sends all needed fields.

--- src/main/java/org/example/models/websocketResponses/GameStartedMessageData.java
+++ src/main/java/org/example/models/websocketResponses/GameStartedMessageData.java
@@ -1,15 +1,21 @@
 package org.example.models.websocketResponses;
 
 import com.google.gson.annotations.Expose;
+import java.util.List;
 import lombok.experimental.SuperBuilder;
 import org.example.entities.Game;
+import org.example.entities.Player;
 
 @SuperBuilder
 public class GameStartedMessageData extends SocketMessageData {
   @Expose Game game;
+  @Expose String gameId;
+  @Expose List<Player> players;
 
   public GameStartedMessageData(Game game) {
     super();
     this.game = game;
+    this.gameId = game.getId();
+    this.players = game.getPlayers();
   }
 }
