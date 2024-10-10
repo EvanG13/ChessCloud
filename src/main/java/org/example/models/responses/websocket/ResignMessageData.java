c59fa848e03698842bc60a2435f21308799e69d7
EvanG13 <emgiampaoli@gmail.com>
2024-10-09 17:41:12 -0700

    feat: resign endpoint (#100)

---
+++ src/main/java/org/example/models/responses/websocket/ResignMessageData.java
@@ -0,0 +1,9 @@
+package org.example.models.responses.websocket;
+
+public class ResignMessageData extends SocketMessageData {
+  private String message;
+
+  public ResignMessageData() {
+    super();
+  }
+}
