20807ae58e9bc65721a5a796dac05ee5dc9758a9
CleveyP <clevelandplonsey@gmail.com>
2024-11-13 04:49:44 -0800

    add draw ResultReasons to enum

--- src/main/java/org/example/enums/ResultReason.java
+++ src/main/java/org/example/enums/ResultReason.java
@@ -12,8 +12,9 @@ public enum ResultReason {
 
   MUTUAL_DRAW("draw by mutual agreement"),
   REPETITION("draw by repetition"),
-  INSUFFICIENT_MATERIAL("draw by insufficient material");
-
+  INSUFFICIENT_MATERIAL("draw by insufficient material"),
+  STALEMATE("draw by stalemate"),
+  FIFTY_MOVE_RULE("draw by 50 move rule");
   private final String message;
 
   ResultReason(String message) {
